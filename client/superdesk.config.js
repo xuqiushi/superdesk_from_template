e39bb944210a67c02a3e5fb13ab9c4760c5e2cd5
qiushi.xu <yingxiongxqs@126.com>
2022-10-11 14:09:21 +0800

    Initialize analytics

---
+++ client/superdesk.config.js
@@ -0,0 +1,41 @@
+/**
+ * This is the default configuration file for the Superdesk application. By default,
+ * the app will use the file with the name "superdesk.config.js" found in the current
+ * working directory, but other files may also be specified using relative paths with
+ * the SUPERDESK_CONFIG environment variable or the grunt --config flag.
+ */
+module.exports = function(grunt) {
+    return {
+        apps: [
+            'superdesk.analytics'
+        ],
+        importApps: [
+            '../index',
+            'superdesk-analytics'
+        ],
+        defaultRoute: '/workspace/personal',
+
+        langOverride: {
+            'en': {
+                'ANPA Category': 'Category',
+                'ANPA CATEGORY': 'CATEGORY'
+            }
+        },
+
+        view: {
+            timeformat: 'HH:mm',
+            dateformat: 'DD.MM.YYYY',
+        },
+
+        features: {
+            preview: 1,
+            swimlane: {defaultNumberOfColumns: 4},
+            editor3: true,
+            validatePointOfInterestForImages: true,
+            editorHighlights: true
+        },
+        workspace: {
+            analytics: true
+        },
+    };
+};
