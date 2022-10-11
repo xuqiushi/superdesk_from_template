9bb8316d90ac9bf73155f43b40837a4bca6772d6
qiushi.xu <yingxiongxqs@126.com>
2022-10-11 14:08:45 +0800

    Initialize prod-kobinet

---
+++ scripts/docker-local-demo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -ue
+
+function dcs() {
+	docker-compose -p sddemo -f ./docker-compose-prebuilt.yml $@
+}
+
+WORK_DIR="$(echo $(cd -P -- "$(dirname -- "$0")" && pwd -P))"
+(test -d $WORK_DIR/env || virtualenv $WORK_DIR/env )
+set +u
+. $WORK_DIR/env/bin/activate
+set -u
+pip install -r $WORK_DIR/../docker/requirements.txt
+
+(
+	docker --version && docker ps >/dev/null && docker-compose --version
+) || (
+	echo "Depended executable not found. Check the message above" && exit 1
+) &&
+
+
+echo '
+|==================================================================|
+|open in browser "http://localhost:8080" after server will be ready|
+|                                                                  |
+|       if you can not log in you probably need to run             |
+|       "./docker-local-create-user.sh"                            |
+|==================================================================|
+'
+
+cd $WORK_DIR/../docker
+dcs kill
+#dcs rm -fv
+dcs pull
+dcs up --timeout 600
