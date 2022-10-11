bd2de1fb23a715a23425d51bf924b53a800775c4
qiushi.xu <yingxiongxqs@126.com>
2022-10-11 14:10:10 +0800

    Initialize superdesk#hotfix/2.3.5

---
+++ client/superdesk.config.js
@@ -0,0 +1,111 @@
+/**
+ * This is the default configuration file for the Superdesk application. By default,
+ * the app will use the file with the name "superdesk.config.js" found in the current
+ * working directory, but other files may also be specified using relative paths with
+ * the SUPERDESK_CONFIG environment variable or the grunt --config flag.
+ */
+module.exports = function() {
+    return {
+        apps: [
+            'superdesk-planning',
+        ],
+        importApps: [
+            '../index',
+            'superdesk-planning',
+        ],
+
+        defaultRoute: '/workspace/monitoring',
+
+        langOverride: {
+            en: {
+                'ANPA Category': 'Category',
+                'ANPA CATEGORY': 'CATEGORY'
+            }
+        },
+
+        view: {
+            timeformat: 'HH:mm',
+            dateformat: 'DD.MM.YYYY',
+        },
+
+        shortTimeFormat: 'HH:mm, DD.MM.YYYY',
+        shortDateFormat: 'HH:mm, DD.MM.YYYY',
+        shortWeekFormat: 'HH:mm, DD.MM.YYYY',
+        startingDay: '1',
+        defaultTimezone: 'Europe/Prague',
+        
+        editor3: { browserSpellCheck: true, },
+        
+        search_cvs: [
+            {id: 'topics', name:'Topics', field: 'subject', list: 'topics'},
+            {id: 'language', name:'Language', field: 'language', list: 'languages'}
+        ],
+
+        features: {
+            preview: 1,
+            swimlane: {columnsLimit: 99},
+            swimlane: {defaultNumberOfColumns: 4},
+            editor3: true,
+            editorHighlights: true,
+            noPublishOnAuthoringDesk: true,
+            customAuthoringTopbar: {
+                toDesk: true,
+                publish: true,
+            },
+            validatePointOfInterestForImages: true,
+            editorHighlights: true,
+            editFeaturedImage: true,
+            searchShortcut: true,
+            elasticHighlight: true,
+            planning: true,
+            autorefreshContent: true,
+            nestedItemsInOutputStage: true,
+            planning: true,
+        },
+        
+        item_profile: { change_profile: 1 },
+
+        workspace: {
+            planning: true,
+            assignments: true,
+        },
+        
+        ui: {
+            italicAbstract: false,
+            },
+
+        list: {
+            priority: [
+                'urgency'
+            ],
+            firstLine: [
+                'headline',  
+                'highlights',
+                'markedDesks',
+                'associatedItems',
+                'versioncreated'
+            ],
+            secondLine: [
+                'state',
+                'update',
+                'scheduledDateTime',
+                'flags',
+                'updated',
+                'provider',
+                'desk',
+                'fetchedDesk',
+                'used',
+                'nestedlink'
+            ]
+        },
+
+        monitoring: {
+            scheduled: {
+                sort: {
+                    default: { field: 'publish_schedule', order: 'asc' },
+                    allowed_fields_to_sort: [ 'publish_schedule' ]
+                }
+            },
+        },   
+    };
+};
