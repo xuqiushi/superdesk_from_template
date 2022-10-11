2984dd48ec09e5cb4c412f57d0ab7863b931e38d
qiushi.xu <yingxiongxqs@126.com>
2022-10-11 14:10:42 +0800

    Initialize code-for-africa

---
+++ client/superdesk.config.js
@@ -0,0 +1,49 @@
+/**
+ * This is the default configuration file for the Superdesk application. By default,
+ * the app will use the file with the name "superdesk.config.js" found in the current
+ * working directory, but other files may also be specified using relative paths with
+ * the SUPERDESK_CONFIG environment variable or the grunt --config flag.
+ */
+module.exports = function(grunt) {
+    return {
+        apps: [
+            'superdesk-planning',
+            'superdesk.analytics',
+        ],
+        importApps: [
+            '../index',
+            'superdesk-planning',
+            'superdesk-analytics',
+        ],
+        defaultRoute: '/workspace/personal',
+
+        langOverride: {
+            'en': {
+                'ANPA Category': 'Category',
+                'ANPA CATEGORY': 'CATEGORY'
+            }
+        },
+
+        view: {
+            timeformat: 'HH:mm',
+            dateformat: 'DD.MM.YYYY',
+        },
+
+        features: {
+            analytics: true,
+            preview: 1,
+            swimlane: {defaultNumberOfColumns: 4},
+            editor3: true,
+            validatePointOfInterestForImages: true,
+            editorHighlights: true,
+            planning: true,
+            customAuthoringTopbar: {
+                toDesk: true,
+            },
+        },
+        workspace: {
+            planning: true,
+            assignments: true,
+        },
+    };
+};
